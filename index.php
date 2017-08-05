31bc2ed3c7502f5ea31eb3818c835024f37b4ad6
starmizi <said.tarmizi@outlook.com>
2017-08-05 15:40:04 +0700

    Add: table header

--- index.php
+++ index.php
@@ -6,11 +6,19 @@
 <body>
 
 <?php
-$json=file_get_contents("http://api.openweathermap.org/data/2.5/forecast/daily?q=Jakarta&mode=json&units=metric&cnt=5&APPID=481e3bc28e5264e5607c2b65b449bfc1");
-$data =  json_decode($json, true);
+$url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Jakarta&mode=json&units=metric&cnt=5&APPID=481e3bc28e5264e5607c2b65b449bfc1";
+$json = file_get_contents($url);
+$data = json_decode($json, true);
 
 print_r($data);
 var_dump($data);
+
+echo "<table>";
+echo "<tr>";
+echo "<th>Jakarta</th>";
+echo "<th>Temperature</th>";
+echo "<th>Variance</th>";
+echo "</table>";
 ?>
 
 </body>
